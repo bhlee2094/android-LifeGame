ef040ea74550906b459f5f3c30bae009b00dbd2a
bh lee <djbh123@naver.com>
2021-04-13 21:02:57 +0900

    Add Firestore

--- app/src/main/java/com/bhlee/myfirstgame/AbilityActivity.java
+++ app/src/main/java/com/bhlee/myfirstgame/AbilityActivity.java
@@ -7,17 +7,30 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class AbilityActivity extends AppCompatActivity implements Button.OnClickListener {
 
     private Button btn_close_ability;
+    private String uid;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ability);
 
+        AbilityInit();
+    }
+
+    private void AbilityInit(){
         btn_close_ability = findViewById(R.id.btn_close_ability);
         btn_close_ability.setOnClickListener(this);
+
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
--- app/src/main/java/com/bhlee/myfirstgame/ItemActivity.java
+++ app/src/main/java/com/bhlee/myfirstgame/ItemActivity.java
@@ -7,17 +7,29 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ItemActivity extends AppCompatActivity implements Button.OnClickListener {
 
     private Button btn_close_item;
+    private String uid;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
+        ItemInit();
+    }
+
+    private void ItemInit(){
         btn_close_item = findViewById(R.id.btn_close_item);
         btn_close_item.setOnClickListener(this);
+
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
